29393cd5c13ce7241968bda7d8c097871a8c4bce
abd elhafid <abdelhafidazligue162@gmail.com>
2026-01-21 11:02:39 +0100

    added the env file for the base url

--- src/public/index.php
+++ src/public/index.php
@@ -3,6 +3,7 @@
 
 // Load router
 require_once __DIR__ . "/../autoload.php";
+require_once __DIR__ . "/../env.php";
 require_once __DIR__ . "/../Routing/Routing.php";
 // Get URI and fix Laragon paths
 $uri = $_SERVER['REQUEST_URI'] ?? '/';
